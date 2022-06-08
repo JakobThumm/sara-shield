bfedc3608584d0e6dc26e2db98f67eb61b07e8ed
JakobThumm <jakob.thumm@web.de>
2022-06-08 17:31:22 +0200

    Added GNU License

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # SaRA-shield
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
 
 This package provides safety for human-robot interaction using reachability analysis.
 We use [SaRA](https://github.com/Sven-Schepp/SaRA) to calculate the reachable sets of humans and robots.
--- safety_shield/include/safety_shield/rviz_marker.h
+++ safety_shield/include/safety_shield/rviz_marker.h
@@ -3,7 +3,15 @@
  * @file rviz_marker.h
  * @brief Defines the rviz visualization class
  * @version 0.1
- * @copyright MIT License. Please see package.xml for further detail.
+ * @copyright This file is part of SaRA-Shield.
+ * SaRA-Shield is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or (at your option) any later version.
+ * SaRA-Shield is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with SaRA-Shield. 
+ * If not, see <https://www.gnu.org/licenses/>. 
  */
 
 #include <exception>
