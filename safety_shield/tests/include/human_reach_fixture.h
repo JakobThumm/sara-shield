e28422cdfa0f9697d7ada3b2d6d091318ee58f16
JakobThumm <jakob.thumm@web.de>
2022-06-13 14:12:48 +0200

    Added human reach measurement and delay error tests

--- safety_shield/tests/include/human_reach_fixture.h
+++ safety_shield/tests/include/human_reach_fixture.h
@@ -31,7 +31,7 @@
 namespace safety_shield {
 
 /**
- * @brief Test fixture for verify long term trajectory class
+ * @brief Test fixture for human reach class
  */
 class HumanReachTest : public ::testing::Test {
  protected:
@@ -45,6 +45,68 @@ class HumanReachTest : public ::testing::Test {
    */
   void SetUp() override {
     std::filesystem::path config_file = std::filesystem::current_path().parent_path() / "config/human_reach_test_single_joint.yaml";
+    YAML::Node human_config = YAML::LoadFile(config_file.string());
+    double measurement_error_pos = human_config["measurement_error_pos"].as<double>();
+    double measurement_error_vel = human_config["measurement_error_vel"].as<double>();
+    double delay = human_config["delay"].as<double>();
+
+    std::vector<std::string> joint_name_vec = human_config["joint_names"].as<std::vector<std::string>>();
+    std::map<std::string, int> joint_names;
+    for(std::size_t i = 0; i < joint_name_vec.size(); ++i) {
+        joint_names[joint_name_vec[i]] = i;
+    }
+
+    std::vector<double> joint_v_max = human_config["joint_v_max"].as<std::vector<double>>();
+    std::vector<double> joint_a_max = human_config["joint_a_max"].as<std::vector<double>>();
+    // Build bodies
+    const YAML::Node& bodies = human_config["bodies"];
+    std::map<std::string, reach_lib::jointPair> body_link_joints;
+    std::map<std::string, double> thickness;
+    for (YAML::const_iterator it = bodies.begin(); it != bodies.end(); ++it) {
+      const YAML::Node& body = *it;
+      body_link_joints[body["name"].as<std::string>()] = reach_lib::jointPair(joint_names[body["proximal"].as<std::string>()], joint_names[body["distal"].as<std::string>()]);
+      thickness[body["name"].as<std::string>()] = body["thickness"].as<double>(); 
+    }
+    // Build extremities
+    const YAML::Node& extremities = human_config["extremities"];
+    std::vector<std::string> extremity_base_names;
+    std::vector<std::string> extremity_end_names; 
+    std::vector<double> extremity_length;
+    for (YAML::const_iterator it = extremities.begin(); it != extremities.end(); ++it) {
+      const YAML::Node& extremity = *it;
+      extremity_base_names.push_back(extremity["base"].as<std::string>());
+      extremity_end_names.push_back(extremity["end"].as<std::string>());
+      extremity_length.push_back(extremity["length"].as<double>());
+    }
+    human_reach_ = new HumanReach(joint_names.size(), 
+      body_link_joints, 
+      thickness, 
+      joint_v_max, 
+      joint_a_max,
+      extremity_base_names, 
+      extremity_end_names, 
+      extremity_length,
+      measurement_error_pos, 
+      measurement_error_vel, 
+      delay);
+  }
+};
+
+/**
+ * @brief Test fixture for human reach class with errors and delay
+ */
+class HumanReachTestError : public ::testing::Test {
+ protected:
+  /**
+   * @brief The human reach object
+   */
+  HumanReach* human_reach_;
+
+  /**
+   * @brief Create the human reach object
+   */
+  void SetUp() override {
+    std::filesystem::path config_file = std::filesystem::current_path().parent_path() / "config/human_reach_test_single_joint_error.yaml";
     YAML::Node human_config = YAML::LoadFile(config_file.string());
     double measurement_error_pos = human_config["measurement_error_pos"].as<double>();
     double measurement_error_vel = human_config["measurement_error_vel"].as<double>();
--- safety_shield/tests/src/human_reach_tests.cc
+++ safety_shield/tests/src/human_reach_tests.cc
@@ -120,6 +120,65 @@ TEST_F(HumanReachTest, HumanReachAnalysisAccTest){
   EXPECT_DOUBLE_EQ(a_cap[0].r_, r);
 }
 
+TEST_F(HumanReachTestError, HumanReachAnalysisVelTestError){
+  reach_lib::Point p(1, 2, 3);
+  std::vector<reach_lib::Point> human_joint_pos;
+  human_joint_pos.push_back(p);
+  double t_meas = 1.0;
+  human_reach_->measurement(human_joint_pos, t_meas);
+  double t_command = 1.01;
+  double t_break = 0.1;
+  double thickness = 0.1;
+  double v_max = 1;
+  human_reach_->humanReachabilityAnalysis(t_command, t_break);
+  double t = (t_command - t_meas) + t_break + human_reach_->getDelay();
+  // radius = thickness + meas_err_pos + ((t_command-t_last) + t_break + t_delay) * v_max 
+  double r = thickness +
+      human_reach_->getMeasurementErrorPos() +
+      t * v_max;
+  std::vector<reach_lib::Capsule> v_cap = human_reach_->getArticulatedVelCapsules();
+  EXPECT_DOUBLE_EQ(v_cap[0].p1_.x, 1.0);
+  EXPECT_DOUBLE_EQ(v_cap[0].p1_.y, 2.0);
+  EXPECT_DOUBLE_EQ(v_cap[0].p1_.z, 3.0);
+  EXPECT_DOUBLE_EQ(v_cap[0].p2_.x, 1.0);
+  EXPECT_DOUBLE_EQ(v_cap[0].p2_.y, 2.0);
+  EXPECT_DOUBLE_EQ(v_cap[0].p2_.z, 3.0);
+  EXPECT_DOUBLE_EQ(v_cap[0].r_, r);
+}
+
+TEST_F(HumanReachTestError, HumanReachAnalysisAccTestError){
+  reach_lib::Point p(1, 2, 3);
+  std::vector<reach_lib::Point> human_joint_pos;
+  human_joint_pos.push_back(p);
+  double t_meas = 1.0;
+  human_reach_->measurement(human_joint_pos, t_meas);
+  reach_lib::Point q(2, 3, 3);
+  human_joint_pos.pop_back();
+  human_joint_pos.push_back(q);
+  t_meas = 2.0;
+  human_reach_->measurement(human_joint_pos, t_meas);
+  double t_command = 2.01;
+  double t_break = 0.1;
+  human_reach_->humanReachabilityAnalysis(t_command, t_break);
+  double t = (t_command - t_meas) + t_break + human_reach_->getDelay();
+  double a_max = 10;
+  double thickness = 0.1;
+  double v0 = sqrt(2);
+  double r = thickness +
+      human_reach_->getMeasurementErrorPos() +
+      human_reach_->getMeasurementErrorVel() * t +
+      0.5 * a_max * pow(t, 2.0);
+  reach_lib::Point next_pos = q + (q-p) * t;
+  std::vector<reach_lib::Capsule> a_cap = human_reach_->getArticulatedAccelCapsules();
+  EXPECT_DOUBLE_EQ(a_cap[0].p1_.x, next_pos.x);
+  EXPECT_DOUBLE_EQ(a_cap[0].p1_.y, next_pos.y);
+  EXPECT_DOUBLE_EQ(a_cap[0].p1_.z, next_pos.z);
+  EXPECT_DOUBLE_EQ(a_cap[0].p2_.x, next_pos.x);
+  EXPECT_DOUBLE_EQ(a_cap[0].p2_.y, next_pos.y);
+  EXPECT_DOUBLE_EQ(a_cap[0].p2_.z, next_pos.z);
+  EXPECT_DOUBLE_EQ(a_cap[0].r_, r);
+}
+
 } // namespace safety_shield
 
 int main(int argc, char **argv){
