a66a96090ec447178e6ebefd4c5acf9dedd26364
JakobThumm <jakob.thumm@tum.de>
2023-05-10 11:27:18 +0200

    Added self-constrained collision checking.

--- safety_shield/src/verify_iso.cc
+++ safety_shield/src/verify_iso.cc
@@ -36,15 +36,44 @@ bool VerifyISO::verify_human_reach(const std::vector<reach_lib::Capsule>& robot_
 bool VerifyISO::clamping_possible(const std::vector<reach_lib::Capsule>& robot_capsules, 
       const std::vector<reach_lib::Capsule>& human_capsules,
       const std::vector<reach_lib::AABB>& environment_elements) {
-  for (const auto& human_capsule : human_capsules) {
-    for (const auto& environment_element : environment_elements) {
-      if (reach_lib::intersections::capsule_aabb_intersection(human_capsule, environment_element)) {
-        for (const auto& robot_capsule : robot_capsules) {
-          if (capsuleCollisionCheck(human_capsule, robot_capsule)) {
-            return true;
-          }
+  // Build a map that maps the robot capsule/environment indices in collision with the human capsules
+  // We choose this indexing to be in line with the paper.
+  std::map<int, std::vector<int>> robot_collision_map;
+  std::map<int, std::vector<int>> environment_collision_map;
+  for (int j = 0; j < human_capsules.size(); j++) {
+    // Collisions with robot links
+    for (int i = 0; i < robot_capsules.size(); i++) {
+      if (capsuleCollisionCheck(human_capsules[j], robot_capsules[i])) {
+        // Robot link i and human body part j could intersect.
+        if (robot_collision_map.find(j) == robot_collision_map.end()) {
+          robot_collision_map[j] = std::vector<int>();
         }
+        robot_collision_map[j].push_back(i);
       }
+    }
+    // Maybe:
+    // if (robot_collision_map.find(j) == robot_collision_map.end()) {
+    //   continue;
+    // }
+    // Collisions with static environment elements
+    for (int k = 0; k < environment_elements.size(); k++) {
+      if (reach_lib::intersections::capsule_aabb_intersection(human_capsules[j], environment_elements[k])) {
+        // Human body part j and environment element k could intersect.
+        if (environment_collision_map.find(j) == environment_collision_map.end()) {
+          environment_collision_map[j] = std::vector<int>();
+        }
+        environment_collision_map[j].push_back(k);
+      }
+    }
+  }
+  for (const auto& robot_collisions : robot_collision_map) {
+    if (robot_collisions.second.size() >= 2) {
+      // Self-constrained collision possible
+      return true;
+    }
+    if (environment_collision_map.find(robot_collisions.first) != environment_collision_map.end()) {
+      // Environment collision possible
+      return true;
     }
   }
   return false;
