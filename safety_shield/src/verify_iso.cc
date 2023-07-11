d89865c0e510f19d910595550782cc93ea8595bc
Leonardo Maglanoc <leo.maglanoc@gmail.com>
2023-07-11 09:05:52 +0200

    debugging

--- safety_shield/src/verify_iso.cc
+++ safety_shield/src/verify_iso.cc
@@ -41,7 +41,9 @@ bool VerifyISO::verify_human_reach(const std::vector<reach_lib::Capsule>& robot_
 int VerifyISO::getIndexOfHead(const std::map<std::string, reach_lib::jointPair>& body_link_joints) {
   if(body_link_joints.count("head") == 1) {
     return body_link_joints.at("head").first;
-  } else if(body_link_joints.count("Head") == 1){
+  } else if(body_link_joints.count("Head") == 1) {
+    spdlog::info("index first is: {}", body_link_joints.at("Head").first);
+    spdlog::info("index second is: {}", body_link_joints.at("Head").second);
     return body_link_joints.at("Head").first;
   } else {
     spdlog::error("VerifyISO::getIndexOfHead: body_link_joints has no head value");
@@ -56,6 +58,9 @@ bool VerifyISO::verify_human_reach_head(const std::vector<reach_lib::Capsule>& r
                              const std::map<std::string, reach_lib::jointPair>& body_link_joints) {
   try {
     int index_head = getIndexOfHead(body_link_joints);
+    // TODO: human_capsules_vel or acc hat index_head nicht?
+    spdlog::info("capsules_vel length is: {}", human_capsules_vel.size());
+    spdlog::info("capsules_acc length is: {}", human_capsules_acc.size());
     bool vel_model = !robotHumanCollision(robot_capsules, {human_capsules_vel.at(index_head)});
     bool acc_model = !robotHumanCollision(robot_capsules, {human_capsules_acc.at(index_head)});
     return vel_model || acc_model;
