3c6b727e1308c4b17cf46ccbe8bce72d091ee1f5
JakobThumm <jakob.thumm@tum.de>
2023-11-21 16:49:33 -0800

    Added first templated Kalman Filter implementation

---
+++ safety_shield/include/safety_shield/measurement_handler.h
@@ -0,0 +1,16 @@
+// -*- lsst-c++ -*/
+/**
+ * @file measurement_handler.h
+ * @brief Defines the MeasurementHandler class
+ * @version 0.1
+ * @copyright This file is part of SaRA-Shield.
+ * SaRA-Shield is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ * SaRA-Shield is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with SaRA-Shield.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
