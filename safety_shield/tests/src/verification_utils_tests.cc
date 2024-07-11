3163051983bc3806a9cf41fb6c05ea5bc0d403cd
JakobThumm <jakob.thumm@tum.de>
2024-07-11 11:12:13 +0200

    Added unit test for FindAllHumanRobotContacts()

--- safety_shield/tests/src/verification_utils_tests.cc
+++ safety_shield/tests/src/verification_utils_tests.cc
@@ -19,6 +19,24 @@ TEST(VerifyTest, RobotHumanCollisionTest) {
   std::vector<reach_lib::Capsule> h_caps = {h_cap1, h_cap2};
   EXPECT_FALSE(robotHumanCollision(r_caps, h_caps));
 }
+
+TEST(VerifyTest, FindAllHumanRobotContactsTest) {
+  reach_lib::Capsule r_cap0(reach_lib::Point(0.0, 0.0, 0.0), reach_lib::Point(0.0, 0.0, 1.0), 0.1);
+  reach_lib::Capsule r_cap1(reach_lib::Point(0.0, 0.0, 1.0), reach_lib::Point(0.0, 0.0, 2.0), 0.1);
+  std::vector<reach_lib::Capsule> r_caps = {r_cap0, r_cap1};
+  reach_lib::Capsule h_cap0(reach_lib::Point(1.0, 0.0, 1.0), reach_lib::Point(0.0, 0.0, 2.0), 0.5);
+  reach_lib::Capsule h_cap1(reach_lib::Point(0.0, 0.0, -0.5), reach_lib::Point(1.0, 0.0, 1.0), 0.5);
+  std::vector<reach_lib::Capsule> h_caps = {h_cap0, h_cap1};
+  std::map<int, std::vector<int>> human_robot_contacts = findAllHumanRobotContacts(h_caps, r_caps);
+  EXPECT_TRUE(human_robot_contacts.find(0) != human_robot_contacts.end());
+  EXPECT_TRUE(human_robot_contacts.find(1) != human_robot_contacts.end());
+  EXPECT_EQ(human_robot_contacts[0].size(), 1);
+  EXPECT_EQ(human_robot_contacts[1].size(), 1);
+  // Human capsule 0 should intersect with robot capsule 1
+  EXPECT_EQ(human_robot_contacts[0][0], 1);
+  // Human capsule 1 should intersect with robot capsule 0
+  EXPECT_EQ(human_robot_contacts[1][0], 0);
+}
 } // namespace safety_shield
 
 int main(int argc, char **argv) {
