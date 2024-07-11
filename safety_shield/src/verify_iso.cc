10910ff3d2b98260c4ab7d8af36358206c839ac8
JakobThumm <jakob.thumm@tum.de>
2024-07-11 13:02:07 +0200

    typo

--- safety_shield/src/verify_iso.cc
+++ safety_shield/src/verify_iso.cc
@@ -4,18 +4,13 @@ namespace safety_shield {
 
 bool VerifyISO::verifyHumanReach(const std::vector<reach_lib::Capsule>& robot_capsules,
                                    std::vector<std::vector<reach_lib::Capsule>> human_capsules) {
-  try {
-    for (const auto& capsule_list : human_capsules) {
-      // If no collision occured, we are safe and don't have to check the rest.
-      if (!robotHumanCollision(robot_capsules, capsule_list)) {
-        return true;
-      }
+  for (const auto& capsule_list : human_capsules) {
+    // If no collision occured, we are safe and don't have to check the rest.
+    if (!robotHumanCollision(robot_capsules, capsule_list)) {
+      return true;
     }
-    return false;
-  } catch (const std::exception& exc) {
-    spdlog::error("Exception in VerifyISO::verifyHumanReach: {}", exc.what());
-    return false;
   }
+  return false;
 }
 
 /// checks safety for each time interval
@@ -25,22 +20,16 @@ bool VerifyISO::verifyHumanReachTimeIntervals(
   int& collision_index
 ) {
   assert(robot_reachable_sets.size() == human_reachable_sets.size());
-  try {
-    for (int i = 0; i < robot_reachable_sets.size(); i++) {
-      if (!verifyHumanReach(robot_reachable_sets[i], human_reachable_sets[i])) {
-        // returns false if at least in one time step, there is a collision
-        collision_index = i;
-        return false;
-      }
+  for (int i = 0; i < robot_reachable_sets.size(); i++) {
+    if (!verifyHumanReach(robot_reachable_sets[i], human_reachable_sets[i])) {
+      // returns false if at least in one time step, there is a collision
+      collision_index = i;
+      return false;
     }
-    // returns true if it was safe in all time intervals
-    collision_index = -1;
-    return true;
-  } catch (const std::exception& exc) {
-    spdlog::error("Exception in VerifyISO::improved_verifyHumanReach: {}", exc.what());
-    collision_index = 0;
-    return false;
   }
+  // returns true if it was safe in all time intervals
+  collision_index = -1;
+  return true;
 }
 
 bool VerifyISO::verifyHumanReachVelocity(const std::vector<std::vector<reach_lib::Capsule>>& robot_reachable_sets,
@@ -57,25 +46,19 @@ bool VerifyISO::verifyHumanReachVelocity(const std::vector<std::vector<reach_lib
   int n_robot_links = robot_reachable_sets[0].size();
   int n_human_models = human_reachable_sets[0].size();
   int n_human_bodies = human_reachable_sets[0][0].size();
-  try {
-    for (int i = 0; i < n_time_intervals; i++) {
-      for (int m = 0; m < n_human_models; m++) {
-        std::map<int, std::vector<int>> human_robot_contacts = findAllHumanRobotContacts(human_reachable_sets[i][m], robot_reachable_sets[i]);
-        bool is_safe = checkVelocitySafety(human_robot_contacts, robot_link_velocities[i], maximal_contact_velocities[m]);
-        if (!is_safe) {
-          collision_index = i;
-          return false;
-        }
+  for (int i = 0; i < n_time_intervals; i++) {
+    for (int m = 0; m < n_human_models; m++) {
+      std::map<int, std::vector<int>> human_robot_contacts = findAllHumanRobotContacts(human_reachable_sets[i][m], robot_reachable_sets[i]);
+      bool is_safe = checkVelocitySafety(human_robot_contacts, robot_link_velocities[i], maximal_contact_velocities[m]);
+      if (!is_safe) {
+        collision_index = i;
+        return false;
       }
     }
-    // returns true if it was safe in all time intervals
-    collision_index = -1;
-    return true;
-  } catch (const std::exception& exc) {
-    spdlog::error("Exception in VerifyISO::verifyHumanReachVelocity: {}", exc.what());
-    collision_index = 0;
-    return false;
   }
+  // returns true if it was safe in all time intervals
+  collision_index = -1;
+  return true;
 }
 
 }  // namespace safety_shield
